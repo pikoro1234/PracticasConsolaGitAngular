d0514fc10df263240c5c6d5fac79f800ccb33f3a
jorge <donjuan.jfc@gmail.com>
2020-07-31 17:27:48 +0200

    creacion de directorio rutas con rutas a paginas diferentes

--- FirstBlog/src/app/app.module.ts
+++ FirstBlog/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ContactoComponent } from './rutas/contacto/contacto.component';
+import { NosotrosComponent } from './rutas/nosotros/nosotros.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ContactoComponent,
+    NosotrosComponent
   ],
   imports: [
     BrowserModule,
